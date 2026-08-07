8e53d46131f7f650bb444ffc4b4d97784ab300b4
maixuanlongnhat20-ctrl <maixuanlongnhat20@gmail.com>
2026-08-07 13:26:58 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,8 +40,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "Chào bạn 👻\n"
     )    
 async def i_love_you(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "I Love Min Ngọt❤️"       
+    await update.message.reply_text("I Love You Min Ngọt❤️")       
     )
 async def admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
